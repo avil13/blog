cbdf811bd0874b05c5d9d763158bb1780dd1df67
Пивкин Алексей Иванович <apivkin@corp.finam.ru>
2023-01-04 12:20:08 +0300

    feat: changed site

--- old/src/_includes/components/Image.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ src, altText = '', caption = '' } = {}) => (`
-  <figure class="media">
-    <img src="${ src }" alt="${ altText }">
-    ${ caption && `
-      <figcaption>${ caption }</figcaption>
-    `}
-  </figure>
-`);
--- old/src/_includes/components/Wrapper.js
+++
@@ -1,5 +0,0 @@
-module.exports = (content = '') => (`
-  <div class="wrapper">
-    ${ content }
-  </div>
-`);
